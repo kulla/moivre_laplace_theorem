71e2a65922aecd38b0460a378a720516d8a51fd3
Stephan Kulla – http://kulla.me/ <stephan.kulla@kulla>
2015-11-11 02:42:04 +0100

    add plots

---
+++ plots/ml_pmf.py
@@ -0,0 +1,18 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+
+from scipy.stats import norm, binom
+
+p = 0.25
+q = 1-p
+n = 20
+
+ks = np.arange(0., n+1, 1.)
+
+xs1 = ( ks - n * p ) / np.sqrt(n*p*q)
+xs2 = np.arange( np.min(xs1), np.max(xs1), 0.01 )
+
+plt.plot(xs1, binom.pmf(ks, n, p), ".")
+plt.plot(xs2, norm.pdf(xs2, 0, 1) / np.sqrt(n * p * q))
+plt.savefig( os.path.splitext(__file__)[0] + ".pdf" )
