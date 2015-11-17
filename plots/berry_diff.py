66c533cd64d342ac1c08de0c758c1d2470fc07fe
Stephan Kulla <stephan@kulla>
2015-11-17 18:38:36 +0100

    update

--- plots/berry_diff.py
+++ plots/berry_diff.py
@@ -6,12 +6,10 @@ from scipy.stats import norm, binom
 
 p = 0.25
 q = 1-p
-n = 1000
+n = 100
 
-ks = np.arange(0., n+1, 1.)
-xs = ( ks - n * p ) / np.sqrt(n*p*q)
+xs= np.arange(-2.5, 2.51, 0.1)
+ks= np.floor(n*p+xs*np.sqrt(n*p*q))
 
-sel = (xs > -5) & (xs < 5)
-
-plt.plot(xs[sel], binom.cdf(ks[sel], n, p)- norm.cdf(xs[sel]))
+plt.plot(xs, binom.cdf(ks, n, p)- norm.cdf(xs), ".")
 plt.savefig( os.path.splitext(__file__)[0] + ".pdf" )
